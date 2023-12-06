81ce935f9f6fb25a00befde3b299f083e637d2f8
Hugo.M <hugo71michel@gmail.com>
2023-12-06 19:09:27 +0100

    ajout du LCD ++

--- SimonV1/SimonV1.ino
+++ SimonV1/SimonV1.ino
@@ -1,11 +1,13 @@
 #include <vector> 
+#include "TM1637.h"
 
 class Touche {
   private : 
   int Bouton;
   int Led;
   int Song;
-  int Buzzer = D6;
+  int Buzzer = D9;
+
 
 
 
@@ -67,12 +69,12 @@ Touche touches[] = {
   Bleu,Rouge,Jaune // on peu directement ajouter les instances de touche ici mais pas les nommer 
 };
 
-int ToucheAppuie(Touche* touches, int numTouches, int attente) {  // pb ici !!!
+int ToucheAppuie(Touche* touches,int attente) {  // pb ici !!!
   unsigned long startMillis = millis(); // Temps de démarrage
   
 
   while ((millis() - startMillis) < attente ) { // si le temps d'attente est supperieur à 1s la fonction verifier reponse n'attends pas le retour (-1) et se met en erreur, relance le programme 
-    for (int i = 0; i < numTouches; i++) {
+    for (int i = 0; i < sizeof(touches); i++) {
       if (touches[i].appuie()) {
         return i; // Renvoie l'indice de la première touche appuyée
       }
@@ -83,8 +85,8 @@ int ToucheAppuie(Touche* touches, int numTouches, int attente) {  // pb ici !!!
 }
 
 
-void genererSequence(std::vector<Touche>& listeTouches, Touche* touches, int nbTouches, int vitesse) { //vitesse de 500 = normal
-  int randomIndex = random(0, nbTouches);
+void genererSequence(std::vector<Touche>& listeTouches, Touche* touches, int vitesse) { //vitesse de 500 = normal
+  int randomIndex = random(0, sizeof(touches));
   listeTouches.push_back(touches[randomIndex]);
 
   for (int i = 0; i < listeTouches.size(); i++) {
@@ -93,17 +95,23 @@ void genererSequence(std::vector<Touche>& listeTouches, Touche* touches, int nbT
   }
 }
 
-bool verifierSequence(const std::vector<Touche>& listeTouches, Touche* touches, int nbTouches, int attente){ //Attention pb attente, voir toucheAppuie{
+bool verifierSequence(const std::vector<Touche>& listeTouches, Touche* touches, int attente){ //Attention pb attente, voir toucheAppuie{
   for (int i = 0; i < listeTouches.size(); i++) {
-    int t = ToucheAppuie(touches, nbTouches, attente) ;
+    int t = ToucheAppuie(touches, attente) ;
     if (t != -1 && touches[t] == listeTouches[i]){ 
 
       listeTouches[i].play();
 
     } else {
-      tone(D6, 200);
+      tone(D9, 200);
       delay(2000);
-      noTone(D6);
+      noTone(D9);
+      delay(250);
+      listeTouches[i].play();
+      delay(250);
+      listeTouches[i].play();
+      delay(250);
+      listeTouches[i].play();
       return false; 
     }
   }
@@ -115,16 +123,28 @@ bool verifierSequence(const std::vector<Touche>& listeTouches, Touche* touches, 
 
 std::vector<Touche> listeTouches;
 
-void setup() {}
+TM1637 Segment(D6, D7);
+int maximum = 12;
+int score;
+
+void setup() {
+Segment.init();
+Segment.set(BRIGHT_TYPICAL);//BRIGHT_TYPICAL = 2,BRIGHT_DARKEST = 0,BRIGHTEST = 7;
+}
 
 void loop() {
 
 disco( touches, 200, 2); // initialisation
+Segment.displayStr("Max");
+delay(100);
+Segment.displayNum(maximum);
 delay(1000);
 
-while (1) {
-  genererSequence(listeTouches, touches, 3, 500);
-  verifierSequence(listeTouches,touches, 3, 1000);
+while (1) { //modedejeuclassique(vitesse de 500)
+  genererSequence(listeTouches, touches, 500);
+  verifierSequence(listeTouches,touches, 1000);
+  score = listeTouches.size();
+  Segment.displayNum(score);
   delay(2000); // attente entre chaque sequence
 }
 }
